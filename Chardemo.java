566ea01ec2b9cfe3d0fdd6c18ef70a5377d4773d
Div882001 <114337996+Div882001@users.noreply.github.com>
2023-02-26 11:11:38 +0530

    Add files via upload

---
+++ Chardemo.java
@@ -0,0 +1,21 @@
+package dayI;
+
+public class Chardemo {
+
+	public static void main(String[] args) {
+		char c='d';
+		System.out.println(c);
+		
+		char c1=75;
+		System.out.println(c1);
+		
+		char var1='\u20AC';
+		System.out.println(var1);
+		
+		char var2='\u00A7';
+		System.out.println(var2);
+		
+
+	}
+
+}
